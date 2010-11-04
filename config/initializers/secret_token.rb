8041aa47ad73f7833894fc903dd5037a5b30fe53
Peter Shih <ptshih@gmail.com>
2010-11-04 02:15:54 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facemash::Application.config.secret_token = '9bfaebc707c11eaed5e9db804d90439f1fbd01225b5f5f8a018fc968994a6bfa6d29d907395da9b6c6c7cf7a3d2d2799fe5a40b6eb0f021f2e5e8f97647480d3'
