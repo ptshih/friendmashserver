a8b0bff8d46ff1996a2882cdefee948bd5afb9f6
gene tsai <sircambridge@gmail.com>
2010-11-06 00:16:13 -0700

    adding random user picker endpoint

--- app/controllers/mash_controller.rb
+++ app/controllers/mash_controller.rb
@@ -1,15 +1,22 @@
 class MashController < ApplicationController
   def random
-    @users = User.all
+    @users = User.all(:order=>'RANDOM()',:limit=>1)
     
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @users }
       format.json  { render :json => @users }
     end
-
+  end
+  
+  def postFriends
+    # upload some users friends to save in the db
+    Rails.logger.info request.query_parameters.inspect
   end
   
   def result
+    # report a match result to the server
+    Rails.logger.info request.query_parameters.inspect
   end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ActiveRecord::Base
+  has_one :profile, :foreign_key => 'facebook_id', :primary_key => 'facebook_id'
 end
