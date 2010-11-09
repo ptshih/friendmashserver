031814cba323199daf42e1f6c87e75ead0b5783d
Peter Shih <ptshih@gmail.com>
2010-11-09 00:35:38 -0800

    adding user -> token has_one relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_one :profile, :foreign_key => 'facebook_id', :primary_key => 'facebook_id'
+  has_one :token, :foreign_key => 'facebook_id', :primary_key => 'facebook_id'
   
   def createWithHash(user)
 
