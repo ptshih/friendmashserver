988447fea0f89ad7a98b5904d53bbc50a9e1b753
Peter Shih <ptshih@gmail.com>
2010-11-10 19:53:44 -0800

    logger

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Facemash::Application.initialize!
-
-config.action_controller.logger = Logger.new(STDOUT)
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,6 @@ Facemash::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.action_controller.logger = Logger.new(STDOUT)
 end
