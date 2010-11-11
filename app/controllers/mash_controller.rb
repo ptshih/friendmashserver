f221dae328758b11e9ff55d94516048fb2c9f426
Peter Shih <ptshih@gmail.com>
2010-11-10 21:45:59 -0800

    s

--- app/controllers/mash_controller.rb
+++ app/controllers/mash_controller.rb
@@ -208,7 +208,7 @@ class MashController < ApplicationController
     
     # Generate first degree network for this user
     generateFirstDegreeNetworkForUser(params[:id], friendIdArray)
-    Delayed::Job.enqueue generateSecondDegreeNetworkForUser(params[:id])
+    enqueue generateSecondDegreeNetworkForUser(params[:id])
     
     # p friendIdArray
     
