13b4fc3a8812342e4529100e2ccfddd2a01f2e74
gene tsai <sircambridge@gmail.com>
2010-11-11 22:17:18 -0800

    adding httpclient gem

---
+++ lib/gender_detector.rb
@@ -0,0 +1,10 @@
+def detectGender(name)
+  html = HTTPClient.new.get_content("http://www.gpeters.com/names/baby-names.php?name=#{name}&button=Go")
+  if html.include?("It's a boy")
+    return 'male'
+  elsif html.include?("It's a girl")
+    return 'female'
+  else
+    return false
+  end
+end
