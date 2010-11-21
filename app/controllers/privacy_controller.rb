dba18fc9d363e18d5c4b3e9366d2ba7fe9d23669
Peter Shih <ptshih@gmail.com>
2010-11-20 17:36:58 -0800

    Added privacy policy and terms of service

--- app/controllers/privacy_controller.rb
+++ app/controllers/privacy_controller.rb
@@ -2,6 +2,7 @@ class PrivacyController < ApplicationController
   # GET /privacy
   # GET /privacy.xml
   def index
+    @title = "Privacy Policy"
     respond_to do |format|
       format.html # index.html.erb
     end
