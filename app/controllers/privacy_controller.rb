79d01515043025e36597335733d363075f81332b
Peter Shih <ptshih@gmail.com>
2010-11-20 18:07:31 -0800

    removing terms/privacy from facemash

--- app/controllers/privacy_controller.rb
+++
@@ -1,9 +0,0 @@
-class PrivacyController < ApplicationController
-  # GET /privacy
-  # GET /privacy.xml
-  def index
-    respond_to do |format|
-      format.html # index.html.erb
-    end
-  end
-end
