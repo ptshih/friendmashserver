0e63f0eb10d754c6eb3a5560f9b8a20c9cd7139f
Peter Shih <ptshih@gmail.com>
2010-11-27 00:36:12 -0800

    code cleanup

--- app/models/name.rb
+++ app/models/name.rb
@@ -1,9 +1,11 @@
 class Name < ActiveRecord::Base
   #
   # Name.all.each do |n| n.delete end
-  # Name.new.loadSomeNames
+  # Name.new.load_names
   # 
-  def loadSomeNames
+  # Used to load names from genders.txt into the Names table in DB
+  # 
+  def load_names
     f = File.open('genders.txt', "r") 
     f.each_line do |line|
       line = JSON.parse("[#{line}]")
