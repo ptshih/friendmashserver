06584803190f9092eb14f140605f50b20eeb1ed0
Peter Shih <ptshih@gmail.com>
2010-12-18 20:36:23 -0800

    hotfix for bounds

--- app/controllers/mash_controller.rb
+++ app/controllers/mash_controller.rb
@@ -542,11 +542,15 @@ class MashController < ApplicationController
       low = 600
       high = 2400
     else
-      bounds = calculate_bounds(desiredScore, population, 1500.0, 282.0, 2500.0)
-      low = bounds[0] - 50
-      high = bounds[1] + 50
+      # bounds = calculate_bounds(desiredScore, population, 1500.0, 282.0, 2500.0)
+      # low = bounds[0] - 50
+      # high = bounds[1] + 50
+      
+      low = desiredScore - 100
+      high = desiredScore + 100
     end
     
+    puts "desiredScore: #{desiredScore}"
     puts "population: #{population}"
     puts "stdDev: #{standardDeviation}"
     puts "sampleSize: #{sampleSize}"
