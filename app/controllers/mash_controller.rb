14da521c32a475c63491f2bc1468e6b975a93c79
Peter Shih <ptshih@gmail.com>
2011-01-11 22:50:45 -0800

    test memcache

--- app/controllers/mash_controller.rb
+++ app/controllers/mash_controller.rb
@@ -1118,7 +1118,7 @@ class MashController < ApplicationController
       mysqlresults.free
 
       response << "If you like friendmash, let us know by rating us on iTunes!"
-      response << "Search 'FriendMash' on Facebook and become a fan!"
+      # response << "Search 'FriendMash' on Facebook and become a fan!"
       
       globalstats = Rails.cache.write('globalstats', response, :expire_in => 1.minutes)
       
